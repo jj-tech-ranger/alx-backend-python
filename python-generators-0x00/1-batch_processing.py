358959a566d9e664de66569a56eb26fd5e92da20
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-07 23:43:53 +0300

    fix: simplify 1-batch_processing.py to ensure no return statement

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -2,7 +2,6 @@
 import seed
 
 def stream_users_in_batches(batch_size):
-    """Generator that fetches rows in batches"""
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT user_id, name, email, age FROM user_data")
@@ -10,7 +9,7 @@ def stream_users_in_batches(batch_size):
     batch = []
     for row in cursor:
         batch.append(row)
-        if len(batch) == batch_size:
+        if len(batch) >= batch_size:
             yield batch
             batch = []
     
@@ -21,7 +20,6 @@ def stream_users_in_batches(batch_size):
     connection.close()
 
 def batch_processing(batch_size):
-    """Processes each batch to filter users over the age of 25"""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
