10da070a30fb9161b123ca680ff0d977dda1d770
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-07 23:31:05 +0300

    fix: rewrite 1-batch_processing.py to use only yield generators

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,21 +1,27 @@
+#!/usr/bin/python3
 import seed
 
 def stream_users_in_batches(batch_size):
+    """Generator that fetches rows in batches"""
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT user_id, name, email, age FROM user_data")
+    
     batch = []
     for row in cursor:
         batch.append(row)
         if len(batch) == batch_size:
             yield batch
             batch = []
+    
     if batch:
         yield batch
+    
     cursor.close()
     connection.close()
 
 def batch_processing(batch_size):
+    """Processes each batch to filter users over the age of 25"""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
