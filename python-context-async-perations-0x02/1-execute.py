3e712f232b5879f2496ecdbf1705945f61f282b6
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-08 10:13:22 +0300

    feat: add ExecuteQuery context manager for task 1

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+class ExecuteQuery:
+    """Context manager that executes a query with parameters"""
+    
+    def __init__(self, db_name, query, params=None):
+        self.db_name = db_name
+        self.query = query
+        self.params = params if params else ()
+        self.connection = None
+        self.cursor = None
+        self.results = None
+    
+    def __enter__(self):
+        """Open connection and execute query"""
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(self.query, self.params)
+        self.results = self.cursor.fetchall()
+        return self.results
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Close cursor and connection"""
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
+        return False
+
+# Example usage
+if __name__ == "__main__":
+    # Execute query with parameter
+    query = "SELECT * FROM users WHERE age > ?"
+    param = (25,)
+    
+    with ExecuteQuery('users.db', query, param) as results:
+        for row in results:
+            print(row)
