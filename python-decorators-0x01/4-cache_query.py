710e622f0329683d89d7165fa79d6f0f7a633e42
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-08 09:39:47 +0300

    feat: add cache_query decorator for task 4

---
+++ python-decorators-0x01/4-cache_query.py
@@ -0,0 +1,49 @@
+4-cache_query.py  import time
+import sqlite3
+import functools
+
+query_cache = {}
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
+            return result
+        finally:
+            conn.close()
+    return wrapper
+
+def cache_query(func):
+    """Decorator that caches query results"""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # Get the query from kwargs
+        query = kwargs.get('query', '')
+        
+        # Check if result is in cache
+        if query in query_cache:
+            print("Using cached result for query:", query)
+            return query_cache[query]
+        
+        # Execute function and cache result
+        print("Executing query and caching result:", query)
+        result = func(*args, **kwargs)
+        query_cache[query] = result
+        
+        return result
+    return wrapper
+
+@with_db_connection
+@cache_query
+def fetch_users_with_cache(conn, query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+#### First call will cache the result
+users = fetch_users_with_cache(query="SELECT * FROM users")
+
+#### Second call will use the cached result
+users_again = fetch_users_with_cache(query="SELECT * FROM users")
